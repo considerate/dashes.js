afdffaff4ed1ccc7ca159f39d31c8b8a6505b573
Christoffer Ackelman <christoffer.ackelman@gmail.com>
2014-08-04 16:20:28 +0200

    Refactored code into functions.

--- dashes.js
+++ dashes.js
@@ -19,32 +19,6 @@ function max(a, b) {
     return a > b ? a : b;
 }
 
-var columnLengths = {};
-rows.forEach(function (row) {
-    Object.keys(row).forEach(function (key) {
-        var str = row[key].toString();
-
-        var length = str.split('\n').map(function (part) {
-            return part.length;
-        }).reduce(max, 0);
-
-        if (!columnLengths[key] || length >= columnLengths[key]) {
-            columnLengths[key] = length;
-        }
-        if (headers[key] === undefined) {
-            headers[key] = key;
-        }
-    });
-});
-
-Object.keys(headers).forEach(function (key) {
-    var str = headers[key].toString();
-    var length = str.length;
-    if (!columnLengths[key] || length >= columnLengths[key]) {
-        columnLengths[key] = length;
-    }
-});
-
 function sum(a, b) {
     return a + b;
 }
@@ -52,12 +26,6 @@ function sum(a, b) {
 function repeat(str, count) {
     return new Array(count + 1).join(str);
 }
-
-var keys = Object.keys(columnLengths);
-
-var totalLength = keys.map(function (key) {
-    return columnLengths[key];
-}).reduce(sum, 0);
 
 function leftPad(text, length) {
     var str = text || '';
@@ -71,46 +39,94 @@ function leftPad(text, length) {
     return str;
 }
 
-var headerKeys = Object.keys(headers);
-
-var topLine = repeat('-', totalLength + keys.length - 1);
-var tableHeader = headerKeys.map(function (key) {
-    return leftPad(headers[key], columnLengths[key]);
-}).join(' ');
-
-var tableHeaderLine = headerKeys.map(function (key) {
-    return repeat('-', columnLengths[key]);
-}).join(' ');
-
-var lines = rows.map(function (row) {
-    var next = row;
-    var current;
-    var lines = [];
-    while (next) {
-        current = next;
-        next = null;
-
-        var line = headerKeys.map(function (key) {
-            var str = current[key] || '';
-            if (typeof str !== 'string') {
-                str = str.toString();
+function generateCell(rows, columnLengths) {
+    return function (key) {
+        var str = rows[0][key] || '';
+        if (typeof str !== 'string') {
+            str = str.toString();
+        }
+        var ind = str.indexOf('\n');
+        if (ind !== -1) {
+            if (!rows[1]) {
+                rows[1] = {};
             }
-            var ind = str.indexOf('\n');
-            if (ind !== -1) {
-                if (!next) {
-                    next = {};
-                }
-                next[key] = str.substring(ind + 1);
-                return leftPad(str.substring(0, ind), columnLengths[key]);
-            } else {
-                return leftPad(str, columnLengths[key]);
-            }
-        }).join(' ');
-
-        lines.push(line);
+            rows[1][key] = str.substring(ind + 1);
+            return leftPad(str.substring(0, ind), columnLengths[key]);
+        } else {
+            return leftPad(str, columnLengths[key]);
+        }
     }
-    return lines.join('\n');
-}).join('\n\n');
+}
 
-var table = [topLine, tableHeader, tableHeaderLine, lines, topLine].join('\n');
-console.log(table);
+function generateRow(columnLengths) {
+    return function(row) {
+        var next = row;
+        var current;
+        var rowLines = [];
+        while (next) {
+            current = next;
+            next = null;
+
+            var array = [current, next];
+
+            var line = Object.keys(columnLengths)
+            .map(generateCell(array, columnLengths))
+            .join(' ');
+
+            next = array[1];
+
+            rowLines.push(line);
+        }
+        return rowLines.join('\n');
+    }
+}
+
+function generateTable(headers, data) {
+    var columnLengths = {};
+    data.forEach(function (row) {
+        Object.keys(row).forEach(function (key) {
+            var str = row[key].toString();
+
+            var length = str.split('\n').map(function (part) {
+                return part.length;
+            }).reduce(max, 0);
+
+            if (!columnLengths[key] || length >= columnLengths[key]) {
+                columnLengths[key] = length;
+            }
+            if (headers[key] === undefined) {
+                headers[key] = key;
+            }
+        });
+    });
+
+    Object.keys(headers).forEach(function (key) {
+        var str = headers[key].toString();
+        var length = str.length;
+        if (!columnLengths[key] || length >= columnLengths[key]) {
+            columnLengths[key] = length;
+        }
+    });
+
+    var keys = Object.keys(columnLengths);
+
+    var totalLength = keys.map(function (key) {
+        return columnLengths[key];
+    }).reduce(sum, 0);
+
+    var topLine = repeat('-', totalLength + keys.length - 1);
+    var tableHeader = keys.map(function (key) {
+        return leftPad(headers[key], columnLengths[key]);
+    }).join(' ');
+
+    var tableHeaderLine = keys.map(function (key) {
+        return repeat('-', columnLengths[key]);
+    }).join(' ');
+
+    var lines = data.map(generateRow(columnLengths)).join('\n\n');
+
+    var table = [topLine, tableHeader, tableHeaderLine, lines, topLine].join('\n');
+    return table;
+}
+
+console.log(generateTable(headers, rows));
